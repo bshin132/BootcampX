02e32e01e05dccc1d8c96e379a9f7043afa5c067
bshin132 <bshin132@gmail.com>
2022-02-09 01:43:56 +0000

    complete exercise 4

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -16,6 +16,9 @@ CREATE TABLE students (
   cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
 );
 
+
+
+
 -- SELECT students.name as student_name, cohorts.name as cohort_name, cohorts.start_date as cohort_start_date, students.start_date as student_start_date
 -- FROM students
 -- INNER JOIN cohorts ON cohorts.id = students.cohort_id 
