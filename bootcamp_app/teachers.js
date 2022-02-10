7c94d1e7c4c84d368e544607b452a80740dd11dc
bshin132 <bshin132@gmail.com>
2022-02-10 23:33:59 +0000

    complete injection exercise

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,23 +1,27 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
 
 const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'bootcampx'
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx",
 });
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
+`;
+pool.query(queryString, values).then((res) => {
+  res.rows.forEach((row) => {
     console.log(`${row.cohort}: ${row.teacher}`);
-  })
+  });
 });
